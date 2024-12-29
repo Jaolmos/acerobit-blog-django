7c66518cfb8647a038bdc3a583b67d0fd3230b24
Jaolmos <jose.a.olmosmartinez@gmail.com>
2024-12-29 18:27:42 +0100

    feat: modelos y admin del blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'categoría'
+        verbose_name_plural = 'categorías'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', kwargs={'slug': self.slug})
+    
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'etiqueta'
+        verbose_name_plural = 'etiquetas'
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+   title = models.CharField(max_length=200)
+   slug = models.SlugField(max_length=200, unique=True)
+   content = models.TextField()
+   created_at = models.DateTimeField(auto_now_add=True) 
+   updated_at = models.DateTimeField(auto_now=True)
+   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+   tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+   class Meta:
+       ordering = ['-created_at']  
+       verbose_name = 'entrada'
+       verbose_name_plural = 'entradas'
+
+   def __str__(self):
+       return self.title
+
+   def get_absolute_url(self):
+       return reverse('blog:post_detail', kwargs={'slug': self.slug})
+    
