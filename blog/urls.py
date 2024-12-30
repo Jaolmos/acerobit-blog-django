ebfcdb7e453baed4ab0b4d772f71a7768610d1f5
Jaolmos <jose.a.olmosmartinez@gmail.com>
2024-12-30 19:12:36 +0100

    feat: filtrado por categorías y tags

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
+    path('category/<slug:slug>/', views.PostByCategoryView.as_view(), name='post_by_category'),
+    path('tag/<slug:slug>/', views.PostByTagView.as_view(), name='post_by_tag'),
     path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,30 @@ class PostDetailView(DetailView):
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
 
+
+class PostByCategoryView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.kwargs['slug']
+        return context
+
+class PostByTagView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.kwargs['slug']
+        return context
+
