c3b5d52d952ddc0b5a8e516f9a29d3331981264b
Jaolmos <jose.a.olmosmartinez@gmail.com>
2024-12-31 19:58:32 +0100

    feat: se corrige paginación

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 10
 
 
 class PostDetailView(DetailView):
