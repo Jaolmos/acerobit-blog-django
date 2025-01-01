9d558eb55c13cb10edb2e006b4876a855d8e556e
Jaolmos <jose.a.olmosmartinez@gmail.com>
2025-01-01 15:49:14 +0100

    feat: integrar TinyMCE como editor de texto enriquecido

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog' 
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('search/', views.SearchResultsView.as_view(), name='search_results'),
+    path('category/<slug:slug>/', views.PostByCategoryView.as_view(), name='post_by_category'),
+    path('tag/<slug:slug>/', views.PostByTagView.as_view(), name='post_by_tag'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    
+    
+    
+    
+]
